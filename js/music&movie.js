33370cc20b8ab100acb825890a3ec53e64e5c6c9
Aimal-Maarij <51042919+Aimal-Maarij@users.noreply.github.com>
2020-12-15 13:01:53 +0100

    music and movie page setup (html, css, js) + connect to the movie api service

---
+++ js/music&movie.js
@@ -0,0 +1,37 @@
+// Initial Values
+const INITIAL_SEARCH_VALUE = 'spiderman';
+const log = console.log;
+
+// Selecting elements from the DOM
+const searchButton = document.querySelector('#search');;
+const searchInput = document.querySelector('#exampleInputEmail1');
+const moviesContainer = document.querySelector('#movies-container');
+const moviesSearchable = document.querySelector('#movies-searchable');
+
+const movieApi = {
+    key : "bcb29f3520fa123edbf8a41e0ae3636f",
+    url : "https://api.themoviedb.org'"
+}
+
+const searchVideo = (event) => {
+    event.preventDefault();
+    
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=bcb29f3520fa123edbf8a41e0ae3636f&query=spider`)
+        .then((response) => {
+            log(response)
+
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' + response.status);
+            return;
+          }
+
+          return response.json();
+        }).then(() => {
+
+    })
+    .catch((err) => {
+      console.log('Fetch Error :-S', err);
+    });
+}
+
+searchButton.addEventListener("click", searchVideo)
