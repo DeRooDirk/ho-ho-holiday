2a94686dcd9ef5a17188edba1e914f4b384d17da
Aimal-Maarij <51042919+Aimal-Maarij@users.noreply.github.com>
2020-12-15 16:42:01 +0100

    add channels search feature to the twitch

--- js/twitchTV.js
+++ js/twitchTV.js
@@ -1,5 +1,30 @@
-new Twitch.Embed("twitch-embed", {
-    width: 1400,
-    height: 800,
-    channel: "Deadlyslob",
+
+const twitchSearchBtn = document.getElementById("twitch_search");
+const twitchInput = document.getElementById("twitch_input");
+const twitchDiv = document.getElementById("twitch-embed")
+let inputValue = "sport";
+
+let twitchEmbed = new Twitch.Embed("twitch-embed", {
+    width: 1000,
+    height: 400,
+    channel: inputValue,
 });
+
+twitchSearchBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    inputValue = "";
+    twitchDiv.innerHTML = "";
+
+    inputValue = twitchInput.value;
+
+    let twitchEmbed = new Twitch.Embed("twitch-embed", {
+        width: 1000,
+        height: 400,
+        channel: inputValue,
+    });
+
+})
+
+
+
