4f19f9f41f09c767da38c15e1a9fa599b7cfabf8
Aimal-Maarij <51042919+Aimal-Maarij@users.noreply.github.com>
2020-12-18 06:38:45 +0100

    movie js-file / recommending movie search based on if a person was good or bad- bad = horror movie- good = romantic movie

--- js/music&movie.js
+++ js/music&movie.js
@@ -6,72 +6,82 @@ const searchButton = document.querySelector('#search');
 const resultDiv = document.querySelector('.movie_details');
 const moviesContainer = document.querySelector('#movies-poster');
 
-let isPersonGood = false;
-
+// Global Variables
+let isPersonGood = true;
 const movieApi = {
-    key: "bcb29f3520fa123edbf8a41e0ae3636f",
-    base: "https://api.themoviedb.org",
-    image_base : "https://image.tmdb.org/t/p/w300/",
-    path : "/search/movie"
+  key: "bcb29f3520fa123edbf8a41e0ae3636f",
+  base: "https://api.themoviedb.org",
+  image_base: "https://image.tmdb.org/t/p/w300/",
+  path: "/search/movie",
+  query: ["horror", "comedy"]
 };
 
-const searchVideo = (event) => {
-    event.preventDefault();
-    moviesContainer.innerHTML="";
-    resultDiv.innerHTML="";
+// Query videos based on person being good or not
+let searchVideo = (data) => {
+  let index = parseInt(Math.floor(Math.random() * data.results.length));
+  log(index)
 
-    fetch(`${movieApi.base}/3${movieApi.path}?api_key=${movieApi.key}&query=horror`)
-        .then((response) => {
-            log(response);
+  for (let i = 0; i < data.results.length; i++) {
+    let movieTitle = document.createElement("h2");
+    movieTitle.innerHTML = data.results[index].title;
 
-            if (response.status !== 200) {
-                console.log('Looks like there was a problem. Status Code: ' + response.status);
-                return;
-            }
+    let movieDescription = document.createElement("p");
+    movieDescription.innerHTML = "<strong> Description: </strong>" + data.results[index].overview;
 
-        // Examine the text in the response
-        response.json().then(function(data) {
-          //console.log(data);
-          //console.log(data.results[3].title);
-          //console.log(data.results[1].overview);
-          console.log(data.results);
-          //console.log(data.results.length);
-          if ( isPersonGood === false){
-            let index = parseInt(Math.floor(Math.random()* data.results.length));
-            log(index)
+    let dateReleased = document.createElement("p");
+    dateReleased.innerHTML = "<strong>  </strong>" + data.results[index].release_date;
 
-            for (let i = 0; i < data.results.length; i++){
+    moviesContainer.style.backgroundImage = "url(" + movieApi.image_base + data.results[index].poster_path + ")";
+    resultDiv.appendChild(movieTitle);
+    resultDiv.appendChild(movieDescription);
+    resultDiv.appendChild(dateReleased);
+    break;
+  }
+}
 
-                //console.log(data.results[index].name);
-                let movieTitle = document.createElement("h2");
-                movieTitle.innerHTML = data.results[index].title;
+const apiCall = (event) => {
+  event.preventDefault();
+  moviesContainer.innerHTML = "";
+  resultDiv.innerHTML = "";
+  if (isPersonGood === true) {
+    fetch(`${movieApi.base}/3${movieApi.path}?api_key=${movieApi.key}&query=${movieApi.query[1]}`)
 
-                let movieDescription = document.createElement("p");
-                movieDescription.innerHTML = "<strong> Description: </strong>" + data.results[index].overview;
+      .then((response) => {
+        log(response);
 
-                let dateReleased = document.createElement("p");
-                dateReleased.innerHTML = "<strong> Description: </strong>" + data.results[index].release_date;
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+        }
 
-
-                moviesContainer.style.backgroundImage = "url(" + movieApi.image_base + data.results[index].poster_path + ")";
-                resultDiv.appendChild(movieTitle);
-                resultDiv.appendChild(movieDescription);
-                resultDiv.appendChild(dateReleased);
-                break;
-
-            
-
-
-
-            }
-          }
-
+        response.json().then(function (data) {
+          searchVideo(data)
         });
       }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
-  
+      )
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err);
+      });
+
+  } else if (isPersonGood === false) {
+    fetch(`${movieApi.base}/3${movieApi.path}?api_key=${movieApi.key}&query=${movieApi.query[0]}`)
+
+      .then((response) => {
+        log(response);
+
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+        }
+
+        response.json().then(function (data) {
+          searchVideo(data)
+        });
+      }
+      )
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err);
+      });
+  }
 }
-searchButton.addEventListener("click", searchVideo)
+searchButton.addEventListener("click", apiCall)
